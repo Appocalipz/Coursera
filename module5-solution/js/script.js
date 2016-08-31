206276594585c5c2969c9fb83ea9ede660fd51a6
Melcom van Eeden <melcomvaneeden@Fortnox.local>
2016-09-01 01:15:30 +0200

    Try 1 XD

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -94,7 +94,7 @@ function buildAndShowHomeHTML (categories) {
     homeHtmlUrl,
     function (homeHtml) {
       var chosenCategoryShortName = chooseRandomCategory(categories);
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName.short_name);
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", "'" + chosenCategoryShortName.short_name + "'");
       insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
     },
     false);
