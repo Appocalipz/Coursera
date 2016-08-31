bae1eff936687acb6629b0a06c84e43105da3cdb
Melcom van Eeden <melcomvaneeden@Fortnox.local>
2016-09-01 00:59:24 +0200

    Try 1 XD

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -97,37 +97,8 @@ function buildAndShowHomeHTML (categories) {
       var chosenCategoryShortName = chooseRandomCategory(categories);
       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "short_name", chosenCategoryShortName);
       insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
-
-        /*
-         var insertProperty = function (string, propName, propValue) {
-         var propToReplace = "{{" + propName + "}}";
-         string = string
-         .replace(new RegExp(propToReplace, "g"), propValue);
-         return string;
-         };
-         */
-
-      // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
-      // chosen category from STEP 2. Use existing insertProperty function for that purpose.
-      // Look through this code for an example of how to do use the insertProperty function.
-      // WARNING! You are inserting something that will have to result in a valid Javascript
-      // syntax because the substitution of {{randomCategoryShortName}} becomes an argument
-      // being passed into the $dc.loadMenuItems function. Think about what that argument needs
-      // to look like. For example, a valid call would look something like this:
-      // $dc.loadMenuItems('L')
-      // Hint: you need to surround the chosen category short name with something before inserting
-      // it into the home html snippet.
-      //
-      // var homeHtmlToInsertIntoMainPage = ....
-
-
-      // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
-      // Use the existing insertHtml function for that purpose. Look through this code for an example
-      // of how to do that.
-      // ....
-
     },
-    false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
+    false);
 }
 
 
